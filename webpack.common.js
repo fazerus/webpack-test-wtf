bf97c7bf3669881b728eae0a8d0c0a16793e477c
Sławomir Rega <slawomir.rega@tlen.pl>
2020-06-04 14:22:26 +0200

    new file: slawek.html

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/slawek.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "slawek.html",
+    }),
   ],
 };
